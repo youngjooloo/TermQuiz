285bb44199344bb28e4b04f073e67abfb320a476
HiKim95 <elderday9572@naver.com>
2022-12-01 16:34:26 +0900

    no message

--- src/main/webapp/resources/ranking/js/memberList.js
+++ src/main/webapp/resources/ranking/js/memberList.js
@@ -2,13 +2,31 @@
  * 
  */
 
-$(function(){
-	$('.removeAdmin').click(function(){
-		location.href="removeadmin?nickname="+this.id;	
+$(function() {
+	$('.removeAdmin').click(function() {
+		location.href = "removeadmin?nickname=" + this.id;
 	});
-	
-	$('.addAdmin').click(function(){
-		location.href="addadmin?nickname="+this.id;	
+
+	$('.addAdmin').click(function() {
+		location.href = "addadmin?nickname=" + this.id;
+	});
+
+	let curDown = false,
+		curXPos = 0;
+
+	$('.table2').on('mousedown', function(e) {
+		curXPos = e.pageX;
+		curDown = true;
+	});
+
+	$(window).mousemove(function(e) {
+		if (curDown) {
+			$('.table-wrap2').scrollLeft(curXPos - e.pageX)
+		}
+	});
+
+	$(window).mouseup(function() {
+		curDown = false;
 	});
 	
 });
